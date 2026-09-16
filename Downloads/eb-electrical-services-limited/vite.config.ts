04b59a723962f8ff623d2890eabb4876e0ef2c01
Ronit <r62544400@gmail.com>
2026-09-16 22:29:01 +0530

    Update vite.config.ts

--- Downloads/eb-electrical-services-limited/vite.config.ts
+++ Downloads/eb-electrical-services-limited/vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/EB-Electrical-Services-Limited/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
